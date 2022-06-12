e7b42f456d380c915a38b5a43b979ca1bcac5170
ZHEKO <fr.artuso@duocuc.cl>
2022-06-11 21:46:33 -0400

    Update de menu y plantilla

--- Web/urls.py
+++ Web/urls.py
@@ -1,8 +1,9 @@
 from unicodedata import name
 from django.urls import URLPattern, path
-from .views import index
+from .views import index, menu
 
 urlpatterns = [
     path('',index,name="index"),
+    path('menu',menu,name="menu"),
 
 ]
--- Web/views.py
+++ Web/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'Web/index.html')
+
+def menu(request):
+    return render(request,'Web/menu.html')
