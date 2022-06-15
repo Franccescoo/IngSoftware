5a4a72a1a1f7269cc4213b5d8cd61090e6bf32fc
Franccesco Artuso <fr.artuso@duocuc.cl>
2022-06-15 12:09:24 -0400

    HTML MAS

--- Web/urls.py
+++ Web/urls.py
@@ -1,6 +1,6 @@
 from unicodedata import name
 from django.urls import URLPattern, path
-from .views import  menu, Listar_Usuario, Registrarse, registrarUsuario, eliminar_usuario
+from .views import  menu, Listar_Usuario, Registrarse, registrarUsuario, eliminar_usuario, test
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -9,9 +9,9 @@ urlpatterns = [
     path('menu',menu,name="menu"),
     path('Listar_Usuario',Listar_Usuario,name="Listar_Usuario"),
     path('Registrarse',views.Registrarse,name="Registrarse"),
+    path('test',test,name="test"),
+
     path('eliminar_usuario',eliminar_usuario, name="eliminar_usuario"),
-
-
     path('registrarUsuario', views.registrarUsuario, name="registrarUsuario"),
 
 ]
